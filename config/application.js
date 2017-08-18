643d5e89ebe3865a787620a7ebacafe7684197f4
Justin Searls <searls@gmail.com>
2017-08-17 22:01:42 -0400

    add standardjs

--- config/application.js
+++ config/application.js
@@ -1,11 +1,20 @@
 module.exports = function(lineman) {
   return {
+    loadNpmTasks: ['grunt-standard'],
+    appendTasks: {
+      common: lineman.config.application.removeTasks.common.concat('standard')
+    },
     removeTasks: {
       common: lineman.config.application.removeTasks.common.concat('jshint')
     },
+    standard: {
+      app: {
+        src: [lineman.config.files.js.app]
+      }
+    },
     watch: {
       lint: {
-        tasks: []
+        tasks: ['standard']
       }
     }
   };
