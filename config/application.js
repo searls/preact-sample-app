228218b59298ff64a861bf25510ec77d5af857f1
Justin Searls <searls@gmail.com>
2017-08-17 21:28:54 -0400

    kill jshint

--- config/application.js
+++ config/application.js
@@ -14,46 +14,8 @@
 module.exports = function(lineman) {
   //Override application configuration here. Common examples follow in the comments.
   return {
-
-    // API Proxying
-    //
-    // During development, you'll likely want to make XHR (AJAX) requests to an API on the same
-    // port as your lineman development server. By enabling the API proxy and setting the port, all
-    // requests for paths that don't match a static asset in ./generated will be forwarded to
-    // whatever service might be running on the specified port.
-    //
-    // server: {
-    //   apiProxy: {
-    //     enabled: true,
-    //     host: 'localhost',
-    //     port: 3000
-    //   }
-    // },
-
-    // Sass
-    //
-    // Lineman supports Sass via grunt-contrib-sass, which requires you first
-    // have Ruby installed as well as the `sass` gem. To enable it, uncomment the
-    // following line:
-    //
-    // enableSass: true,
-
-    // Asset Fingerprints
-    //
-    // Lineman can fingerprint your static assets by appending a hash to the filename
-    // and logging a manifest of logical-to-hashed filenames in dist/assets.json
-    // via grunt-asset-fingerprint
-    //
-    // enableAssetFingerprint: true,
-
-    // LiveReload
-    //
-    // Lineman can LiveReload browsers whenever a file is changed that results in
-    // assets to be processed, preventing the need to hit F5/Cmd-R every time you
-    // make a change in each browser you're working against. To enable LiveReload,
-    // comment out the following line:
-    //
-    // livereload: true
-
+    removeTasks: {
+      common: ['jshint']
+    }
   };
 };
