70f857994ebf6db43e7298fde3465567fd5aece3
Justin Searls <searls@gmail.com>
2017-08-20 09:41:53 -0400

    Add in-memory persistence to refresh pushState URL
    * app.store singletons act as an in-memory caching layer to only fetch
    sentences and queries once if they're ever repeated
    * split from 2 to 3 routes because the only thing /search/:query and /
    have in common is a search bar at the bottom, so I extracted it out
    * no more XHRs in views, the stores will do that and only when necessary
    * handling async was a challenge because I want to render something 
    synchronously even when I don't have data, so the store search methods
    will both return a value (either the result or nothing) _and_ take an
    optional callback. This works fine, but is very unusual in JS/node-land
    so will throw people off, especially people who'd be all "yo just use a
    promise", which would defeat the purpose of having a sync response
    
    Best of all, it looks like refreshing the page successfully re-hydrates
    all the templates.

--- app/js/main.js
+++ app/js/main.js
@@ -1,51 +1,123 @@
 window.h = preact.h
+preact.router = window.preactRouter
 window.app = window.app || {}
+
+// app.store -  In-memory persistence stuff
+app.store = app.store || {}
+app.store.Base = class Store {
+  static get instance () {
+    if (this._instance) return this._instance
+    this._instance = new this()
+    return this._instance
+  }
+
+  constructor () {
+    this.stuff = new Map()
+  }
+
+  get (key) {
+    return this.stuff.get(key)
+  }
+
+  set (key, value) {
+    return this.stuff.set(key, value)
+  }
+
+  has (key) {
+    return this.stuff.has(key)
+  }
+}
+
+app.store.Sentence = class SentenceStore extends app.store.Base {
+  find (id, optionalCb) {
+    if (this.has(id)) {
+      return this.get(id)
+    } else {
+      fetch(`/api/sentence/${id}`).then(res => res.json()).then(json => {
+        this.set(id, json.sentence)
+        optionalCb(null, json.sentence)
+      })
+      return null
+    }
+  }
+
+  triage (sentences) {
+    sentences.forEach(sentence => this.set(sentence.id, sentence))
+    return sentences
+  }
+}
+
+app.store.Search = class SearchStore extends app.store.Base {
+  execute (query, optionalCb) {
+    if (this.has(query)) {
+      return this.get(query)
+    } else {
+      fetch(`/api/search/${query}`).then(res => res.json()).then(json => {
+        const sentences = app.store.Sentence.instance.triage(json.sentences)
+        this.set(query, sentences)
+        optionalCb(null, sentences)
+      })
+      return []
+    }
+  }
+}
+
+// app.component - view stuff
 app.component = app.component || {}
 
-app.component.main = ({query, results, selected, onChange}) =>
-  h(window.preactRouter, {id: 'app'},
+app.component.main = ({query, results, sentenceId, onChange}) =>
+  h(preact.router, {},
     h(app.component.show, {
-      path: '/sentence/:selected',
+      path: '/sentence/:sentenceId',
       query,
-      results,
-      onGoBack: () => onChange({query, results})
+      onChange
     }),
     h(app.component.index, {
-      default: true,
-      query,
+      path: '/search/:query',
       results,
       onChange
-    })
+    }),
+    h(app.component.welcome, { default: true })
   )
 
-app.component.show = ({selected, query, results, onGoBack}) => {
-  const sentence = results.find(result => String(result.id) === String(selected))
-  return h('div', {class: 'sentence'},
-    h('div', {}, sentence.japanese),
-    h('div', {}, sentence.english),
-    h('a', {href: `japanese://search/${sentence.japanese}`}, 'Search in Japanese.app'),
-    h('br'),
-    h('a', {href: `midori://translate?text=${sentence.japanese}`}, 'Translate in Midori.app'),
+app.component.show = ({sentenceId, query, onChange}) => {
+  const sentence = app.store.Sentence.instance.find(sentenceId, (er, sentence) => {
+    onChange({sentence, sentenceId, query, error: er})
+  })
+
+  return h('div', {},
+    sentence ? h(app.component.sentence, {sentence}) : 'Loading…',
     h('div', {class: 'go-back'},
-      h('a', {href: `/search/${query}`}, '<-'),
-      query
+      h('a', {href: query ? `/search/${query}` : '/'}, ' <- '),
+      query || 'Back to search'
     )
   )
 }
 
-app.component.index = ({query, results, onChange}) =>
-  h('div', {},
-    query ? h(app.component.results, {results}) : h(app.component.instructions, {}),
-    h(app.component.search, {
-      query,
-      onSearch: (query) => {
-        if (!query) return onChange({})
-        fetch(`/api/search/${query}`).then(res => res.json()).then(json => {
-          onChange({query: json.query, results: json.results})
-        })
-      }
-    })
+app.component.sentence = ({sentence}) =>
+  h('div', {class: 'sentence'},
+    h('div', {}, sentence.japanese),
+    h('div', {}, sentence.english),
+    h('ul', {class: 'actions'},
+      h('li', {},
+        h('a', {href: `japanese://search/${sentence.japanese}`}, 'Search in Japanese.app')
+      ),
+      h('li', {},
+        h('a', {href: `midori://translate?text=${sentence.japanese}`}, 'Translate in Midori.app')
+      )
+    )
   )
+
+app.component.index = ({query, results, onChange}) => {
+  results = app.store.Search.instance.execute(query, (er, sentences) => {
+    onChange({query, results: sentences, error: er})
+  })
+
+  return h('div', {},
+    h(app.component.results, {results}),
+    h(app.component.search, {query})
+  )
+}
 
 app.component.results = ({query, results}) =>
   h('ul', {class: 'results'},
@@ -57,16 +129,29 @@ app.component.result = ({japanese, id}) =>
     h('a', {href: `/sentence/${id}`}, japanese)
   )
 
+app.component.welcome = () =>
+  h('div', {},
+    h(app.component.instructions, {}),
+    h(app.component.search, {})
+  )
+
 app.component.instructions = () =>
   h('div', {class: 'instructions'}, `
     To get started, just search for a token or something.
   `)
 
-app.component.search = ({query, onSearch}) =>
+app.component.search = ({query}) =>
   h('input', {type: 'search',
     placeholder: 'Type words…',
     value: query,
-    onKeyUp: (e) => onSearch(e.target.value)
+    onKeyUp: (e) => {
+      const inputValue = e.target.value
+      if (inputValue) {
+        preact.router.route(`/search/${inputValue}`)
+      } else {
+        preact.router.route('/')
+      }
+    }
   })
 
 app.render = (props) => {
