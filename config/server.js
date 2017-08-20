2cbab3b8a44d0da3c2424bad48818326e3ae98ef
Justin Searls <searls@gmail.com>
2017-08-20 09:25:20 -0400

    create a route for fetching individual sentences
    * Also modify the fake sentence each query to make sure that i'm seeing
    the results for the right query and fetched only when needed
    * Also change to stringy IDs

--- config/server.js
+++ config/server.js
@@ -1,16 +1,28 @@
 var _ = require('lodash')
 
+const SENTENCES = [
+  {id: 'lol', japanese: 'fake sentence', english: 'No translation necessary'},
+  {id: '22', japanese: '知らない', english: 'I dunno'},
+  {id: '202', japanese: 'ウンコ', english: 'Poop'},
+  {id: '212', japanese: '大阪弁が変だろう', english: 'Osaka dialect is strange'}
+]
+
 module.exports = {
   drawRoutes: function(app) {
-    app.get('/api/search/:query', function(req, res){
+    app.get('/api/search/:query', function(req, res) {
+      // Hack to verify visually results are updating & caching
+      SENTENCES.find(s => s.id === 'lol').japanese = 'last query was: ' + req.params.query + ' at ' + new Date()
+
       res.json({
         query: req.params.query,
-        results: _.shuffle([
-          {id: -1, japanese: '「q=' + req.params.query + '」', english: 'No translation necessary'},
-          {id: 22, japanese: '知らない', english: 'I dunno'},
-          {id: 202, japanese: 'ウンコ', english: 'Poop'},
-          {id: 212, japanese: '大阪弁が変だろう', english: 'Osaka dialect is strange'}
-        ])})
-    });
+        sentences: _.shuffle(SENTENCES)
+      })
+    })
+
+    app.get('/api/sentence/:id', function (req, res) {
+      res.json({
+        sentence: SENTENCES.find(sentence => sentence.id === req.params.id)
+      })
+    })
   }
-};
+}
